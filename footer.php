548931b72a800b278847f17a6d720c9a3b1e899c
dela0331dela0331@algonquinlive.com <yournew@email.com>
2024-03-14 19:15:28 -0400

    fixes footer

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 <footer>
     <h3>All right  reserved @2024</h3>
-    <?php echo addNum(3,9); ?>
+    <?php echo addNum(3,9); 
+    
+    $footerLinks = array(`<a href="https://banff.ca/">Old Site</a>`, `<a href="https://education.nationalgeographic.org/resource/erosion/">Science</a>`, `<a href="https://volcano.si.edu/gvp_currenteruptions.cfm">More science</a>`);
+
+foreach ($footerLinks as $linkArray) {
+    echo "$linkArsray | ";
+  }?>
 </footer>
 <?php wp_footer();
 
-$footerLinks = array(`<a href="https://banff.ca/">Old Site</a>`, `<a href="https://education.nationalgeographic.org/resource/erosion/">Science</a>`, `<a href="https://volcano.si.edu/gvp_currenteruptions.cfm">More science</a>`);
 
-foreach ($footerLinks as $linkArray) {
-    echo "$linkArray | ";
-  }
 ?> 
 </body>
 </html>
