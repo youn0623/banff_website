db90e5a0fe5b8d31a71d879320d1221c44b37464
dela0331dela0331@algonquinlive.com <yournew@email.com>
2024-03-14 18:59:21 -0400

    changes footer

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
     <h3>All right  reserved @2024</h3>
     <?php echo addNum(3,9); ?>
 </footer>
-<?php wp_footer(); 
+<?php wp_footer();
 
 $footerLinks = array(`<a href="https://banff.ca/">Old Site</a>`, `<a href="https://education.nationalgeographic.org/resource/erosion/">Science</a>`, `<a href="https://volcano.si.edu/gvp_currenteruptions.cfm">More science</a>`);
 
 foreach ($footerLinks as $linkArray) {
     echo "$linkArray | ";
   }
-?>
+?> 
 </body>
 </html>
